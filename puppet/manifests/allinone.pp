05668c3a3b9e23633a7e6835879465cbad7747c7
Mark Burnett <mark.m.burnett@gmail.com>
2014-09-14 06:05:05 -0500

    puppet: move auth web to new file

--- puppet/manifests/allinone.pp
+++ puppet/manifests/allinone.pp
@@ -35,22 +35,7 @@ class {'rabbitmq':
 }
 
 
-# --- Auth ---
-$sig_key = hiera('auth-signature-key')
-$auth_pass = hiera('auth-postgres-password')
-ptero::web{'auth':
-  code_dir    => '/var/www/auth',
-  source      => hiera('auth-repo'),
-  revision    => hiera('auth-tag'),
-  listen_port => hiera('auth-port'),
-  app         => 'puppet:///modules/ptero/auth/app.py',
-  environment => {
-    'SIGNATURE_KEY' => "$sig_key",
-    'DATABASE_URL'  => "postgres://ptero_auth:$auth_pass@localhost/ptero_auth",
-    'AUTH_URL'      => 'http://192.168.10.10',
-  },
-  require     => Postgresql::Server::Db['ptero_auth'],
-}
+class {'ptero::auth::web': }
 
 
 # -- Shell command ---
