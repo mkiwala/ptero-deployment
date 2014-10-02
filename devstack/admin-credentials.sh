a460f80d57d0afef0a23ebcf198b52a2a174186a
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-02 14:56:46 -0500

    remove devstack Vagrant code

--- devstack/admin-credentials.sh
+++
@@ -1,5 +0,0 @@
-export OS_USERNAME=admin
-export OS_PASSWORD=password
-export OS_PROJECT_NAME=admin
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://192.168.50.50:5000/v2.0
--- devstack/credentials.sh
+++
@@ -1,5 +0,0 @@
-export OS_USERNAME=demo
-export OS_PASSWORD=password
-export OS_PROJECT_NAME=demo
-export OS_TENANT_NAME=demo
-export OS_AUTH_URL=http://${DEVSTACK_IP:-192.168.50.50}:5000/v2.0
