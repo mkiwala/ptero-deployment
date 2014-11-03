ff53bdadc96bc0e2043fc5af2acf7c4bcb2f7793
Mark Burnett <mark.m.burnett@gmail.com>
2014-11-03 10:47:14 -0600

    add start and stop commands for timing ptero benchmarks

--- benchmark/puppet/benchmarking/manifests/init.pp
+++ benchmark/puppet/benchmarking/manifests/init.pp
@@ -3,11 +3,31 @@ class benchmarking() {
         ensure => present,
     }
 
+    package{'bc':
+        ensure => present,
+    }
+
+    file {'/usr/local/bin/benchmark_start':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0555',
+        source  => 'puppet:///modules/benchmarking/benchmark_start',
+    }
+
     file {'/usr/local/bin/benchmark_sleep':
         ensure  => present,
         owner   => root,
         group   => root,
         mode    => '0555',
         source  => 'puppet:///modules/benchmarking/benchmark_sleep',
+    }
+
+    file {'/usr/local/bin/benchmark_stop':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0555',
+        source  => 'puppet:///modules/benchmarking/benchmark_stop',
     }
 }
